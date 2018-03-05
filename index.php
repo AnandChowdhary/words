90990324d6226725556f033277c3db38ecaecc07
Anand Chowdhary <anandchowdhary@gmail.com>
2018-03-05 10:27:23 +0100

    Add option to save post and openssl

--- index.php
+++ index.php
@@ -17,13 +17,30 @@
 		exit(0);
 	}
 
-	$GLOBALS["key"] = "example_key";
+	$GLOBALS["key"] = "example_secure_key";
+	$GLOBALS["iv"] = "example_initialization_vector";
+	$GLOBALS["password"] = "example_password";
+
+	// https://gist.github.com/joashp/a1ae9cb30fa533f4ad94
+	function encrypt_decrypt($action, $string) {
+		$output = false;
+		$encrypt_method = "AES-256-CBC";
+		$key = hash("sha256", $GLOBALS["key"]);
+		$iv = substr(hash("sha256", $GLOBALS["iv"]), 0, 16);
+		if ($action == "encrypt") {
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		} else if($action == "decrypt") {
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+		return $output;
+	}
 
 	Flight::route("POST /", function() {
 		header("Content-Type: application/json");
 		$data = json_decode(file_get_contents("php://input"));
 		if (json_last_error() === JSON_ERROR_NONE) {
-			if ($data->password === "password") {
+			if ($data->password === $GLOBALS["password"]) {
 				$token = [
 					"expires" => (new DateTime("now + 25 hours"))->format("Y-m-d H:i:s")
 				];
@@ -32,22 +49,21 @@
 					"version" => "4.1",
 					"token" => JWT::encode($token, $GLOBALS["key"]),
 					"expires" => (new DateTime("now + 25 hours"))->format("Y-m-d H:i:s")
-				], JSON_PRETTY_PRINT);
+				], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 			} else {
 				echo json_encode([
 					"api" => "words",
 					"version" => "4.1",
 					"error" => "Invalid password"
-				], JSON_PRETTY_PRINT);
+				], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 			}
 		}
 	});
 
-	Flight::route("POST /posts", function() {
+	Flight::route("GET /posts", function() {
 		header("Content-Type: application/json");
-		$data = json_decode(file_get_contents("php://input"));
 		if (json_last_error() === JSON_ERROR_NONE) {
-			$decoded = JWT::decode($data->token, $GLOBALS["key"], array("HS256"));
+			$decoded = JWT::decode(isset($_SERVER["HTTP_TOKEN"]) ? $_SERVER["HTTP_TOKEN"] : null, $GLOBALS["key"], array("HS256"));
 			if ($decoded && ($decoded->expires > (new DateTime("now"))->format("Y-m-d H:i:s"))) {
 				$files = [];
 				$metas = array_diff(scandir("./words"), array(".", ".."));
@@ -60,14 +76,57 @@
 				echo json_encode([
 					"api" => "words",
 					"version" => "4.1",
-					"result" => $files
-				], JSON_PRETTY_PRINT);
+					"result" => $files,
+				], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 			} else {
 				echo json_encode([
 					"api" => "words",
 					"version" => "4.1",
 					"error" => "Unauthenticated"
-				], JSON_PRETTY_PRINT);
+				], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+			}
+		}
+	});
+
+	Flight::route("PUT /posts", function() {
+		header("Content-Type: application/json");
+		if (json_last_error() === JSON_ERROR_NONE) {
+			$decoded = JWT::decode(isset($_SERVER["HTTP_TOKEN"]) ? $_SERVER["HTTP_TOKEN"] : null, $GLOBALS["key"], array("HS256"));
+			if ($decoded && ($decoded->expires > (new DateTime("now"))->format("Y-m-d H:i:s"))) {
+				$data = json_decode(file_get_contents("php://input"));
+				$error = false;
+				if (!isset($data->title)) {
+					$error = true;
+				}
+				if (!isset($data->body)) {
+					$error = true;
+				}
+				if (!$error) {
+					$post = [
+						"title" => encrypt_decrypt("encrypt", $data->title),
+						"date" => (new DateTime("now"))->format("Y-m-d H:i:s"),
+						"body" => encrypt_decrypt("encrypt", $data->body)
+					];
+					file_put_contents("./words/{$data->date}-" . substr(md5($data->date), 0, 10) . ".json", json_encode($post, JSON_UNESCAPED_SLASHES));
+					echo json_encode([
+						"api" => "words",
+						"version" => "4.1",
+						"created" => true,
+						"post" => $post,
+					], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+				} else {
+					echo json_encode([
+						"api" => "words",
+						"version" => "4.1",
+						"error" => "field"
+					], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+				}
+			} else {
+				echo json_encode([
+					"api" => "words",
+					"version" => "4.1",
+					"error" => "Unauthenticated"
+				], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
 			}
 		}
 	});
